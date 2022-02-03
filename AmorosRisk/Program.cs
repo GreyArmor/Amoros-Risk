3d45ac92c5835b071a1ad19df151a4f0e65c0663
GreyArmor <greyarmor@gmail.com>
2022-02-03 04:08:41 +0200

    initial commit!

---
+++ AmorosRisk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AmorosRisk
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new AmorosRiskGame())
+				game.Run();
+		}
+	}
+}
